3121765af3e13e2dc95a3fdf1b99ab7915fcc834
Karan Karakoti <karankarakoti@gmail.com>
2020-05-16 10:31:26 +0530

    deploy

--- client/src/components/pages/Landing.js
+++ client/src/components/pages/Landing.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Landing=() =>(
     
         <div style={{textAlign:'center'}}>
-        <h1>Selling you great stuff</h1>
-        with the help of chatbot
+        <h1>This chatbot is in its training Phase </h1>
+        <h2>So don't mind its answers</h2>
         </div>
     )
 
