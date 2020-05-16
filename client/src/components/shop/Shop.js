9533ed9473a1ebc1054f51ed12db00bc25a8328e
Karan Karakoti <karankarakoti@gmail.com>
2020-05-16 11:50:51 +0530

    deploy

--- client/src/components/shop/Shop.js
+++ client/src/components/shop/Shop.js
@@ -4,7 +4,7 @@ import ItemList from './ItemList';
 const Shop=() =>(
         <div>
         <h2>Here is the github link of the project</h2>
-        
+        <h3>https://github.com/karankarakoti/Chatbot.git</h3>
         </div>
     )
 
