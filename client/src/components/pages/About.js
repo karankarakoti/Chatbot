441a96c87e310b8d7f6522353c9c1a79d8026b4e
Karan Karakoti <karankarakoti@gmail.com>
2020-05-16 11:53:12 +0530

    deploy

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -3,6 +3,7 @@ import React from 'react';
 const About=() =>(
         <div>
         <h2>This project is build on NODE JS and Dialogflow</h2>
+        <h2>This project is deployed in Heroku</h2>
         </div>
     )
 
--- client/src/components/shop/Shop.js
+++ client/src/components/shop/Shop.js
@@ -5,6 +5,7 @@ const Shop=() =>(
         <div>
         <h2>Here is the github link of the project</h2>
         <h3>https://github.com/karankarakoti/Chatbot.git</h3>
+        <h4>You need to install additional modules. You can find those modules in package.json file</h4>
         </div>
     )
 
